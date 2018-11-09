c05f36c7a7040fa982602bd202bd7a4dc12217f6
Iglan Cardeal <cmtcardeal@outlook.com>
2018-11-08 23:01:10 -0300

    Refatoração da classe CarsDAO para evitar SQL injection.

--- app/models/CarsDAO.js
+++ app/models/CarsDAO.js
@@ -1,13 +1,19 @@
 // esta é a classe que realiza a manipulação no banco de listar, inserir e mover um registro.
+class CarsDAO{
 
-function CarsDAO(connection){
-  this.connection = connection;
-  //listar
-  this.listar = (callback) => this.connection.query('SELECT * FROM tb_cars', callback);
-  //inserir;
-  this.inserir = (car, callback) => this.connection.query('INSERT INTO tb_cars SET ?',car , callback);
-  //remover;
-  this.remover = (car, callback) => this.connection.query('DELETE FROM tb_cars WHERE carID = ' + car.carID, callback);
+  constructor(connection){
+    this.connection = connection;
+    //listar
+    this.listar = (callback) => this.connection.query('SELECT * FROM tb_cars', callback);
+    //inserir;
+    this.inserir = (car, callback) => this.connection.query('INSERT INTO tb_cars SET ?',car , callback);
+    //remover;
+    this.remover = (car, callback) => {
+      const sql = 'DELETE FROM tb_cars WHERE carID = ?';
+      this.connection.query(sql, car.carID, callback);
+    };
+  }
+
 }
 
 module.exports = _ => CarsDAO;
